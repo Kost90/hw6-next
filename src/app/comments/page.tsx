6597dd633c49e41cf1919d36b36ebb2eed303880
kost90 <kostdonc90@gmail.com>
2023-11-01 23:20:32 +0000

    created comments page

--- src/app/comments/page.tsx
+++ src/app/comments/page.tsx
@@ -1,10 +1,27 @@
 import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import JsonPlaceholderAPI from '@/api/JsonPlaceholderAPI/JsonPlaceholderAPI';
 
-const Comments = () => {
+const Comments = async () => {
+const comments = await JsonPlaceholderAPI.getComments({signal:new AbortController().signal, cache: 'no-cache'})
+
   return (
-    <div>
-      Comments
-    </div>
+    <>
+      <Typography variant="h4" gutterBottom>
+        Comments
+      </Typography>
+      <List>
+      {comments.map(coment => (
+        <ListItem key={coment.id} alignItems="flex-start">
+          <ListItemText primary={coment.name} secondary={
+                <>
+                  <Typography variant="subtitle1">{coment.email}</Typography>
+                  {coment.body}
+                </>
+              }/>
+        </ListItem>
+      ))}
+      </List>
+      </>
   );
 };
 
